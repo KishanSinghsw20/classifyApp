e84794cb65c6e14ce4743664c19283a37d095029
Kishan.Singh <Kishan.Singh@goolean.tech>
2025-05-03 21:53:40 +0530

    add new files and changes in the data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={'/'} exact={true} element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
+    // Header Start
     <header>
-      <div className="top-strip py-2">
+      {/* Top Strip Start */}
+      <div className="top-strip py-2 border-t-[1px] border-b-[1px] border-gray-300">
         <div className="container">
           <div className="flex items-center justify-between">
             <div className="col1 w-[50%]">
-              <p className="text-[14px] font-[500]">
+              <p className="text-[12px] font-[500]">
                 Get upto 50% off new season styles, limited time only
               </p>
+            </div>
+            <div className="flex items-center justify-end">
+              <ul className="flex items-center gap-3">
+                <li className="list-none">
+                  <Link
+                    to="/help-center"
+                    className="text-[12px] link font-[500] transition"
+                  >
+                    Help Center
+                  </Link>
+                </li>
+                |
+                <li className="list-none">
+                  <Link
+                    to="/order-tracking"
+                    className="text-[12px] link font-[500] transition"
+                  >
+                    Order Tracking
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+      {/* Top Strip End */}
+
+      {/* Main Header Start */}
+      <div className="header">
+        <div className="container flex items-center justify-between">
+          <div className="col1"></div>
+          <div className="col2"></div>
+          <div className="col3"></div>
+        </div>
+      </div>
+      {/* Main Header End */}
     </header>
+    // Header End
   );
 };
 
